6d257f1fff6fb99514adb32799c0c5cff6a5dfe3
Einar Bragi Guðmundsson <einarbragi@snerpa.is>
2018-09-04 15:51:30 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,3 @@
+# Design an algorithm that generates the first n numbers in the following sequence:; 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
