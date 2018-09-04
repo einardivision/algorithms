2bf8e79fb5fd60aed9a1660c8f21ca16113080c7
Einar Bragi Guðmundsson <einarbragi@snerpa.is>
2018-09-04 18:05:35 +0000

    Algorithm for sequence complete

--- sequence.py
+++ sequence.py
@@ -1,3 +1,15 @@
 # Design an algorithm that generates the first n numbers in the following sequence:; 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
-
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+nums = [1,2,3]
+
+for number in nums:
+    print(number)
+    if number == n:
+        break
+
+for i in range(n):
+    num = nums[-1] + nums[-2] + nums[-3]
+    nums.append(num)
+    print(num)
+    if n == len(nums):
+        break
